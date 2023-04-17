b11791d61134c68515c04bef805b93f352f2a3f8
SoulAndBone <soulandbone@gmail.com>
2023-04-17 14:09:43 +0300

    added Utils folder for AppStyles with a couple of Styles

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
-
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:trivia_app/utils/app_styles.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,11 +9,18 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppStyles.bgColor,
       appBar: AppBar(
         title: const Text('Quiz App'),
       ),
-      body: const Center(
-        child: Text('This is the Home Screen'),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(children: const [
+          Text(
+            'Hi, John',
+            style: AppStyles.titleStyle,
+          )
+        ]),
       ),
     );
   }
--- lib/utils/app_styles.dart
+++ lib/utils/app_styles.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppStyles {
-  static const Color = Colors.orange;
+  static const Color bgColor = Color.fromRGBO(60, 67, 89, 1);
+  static const TextStyle titleStyle =
+      TextStyle(fontWeight: FontWeight.bold, fontSize: 24);
 }
