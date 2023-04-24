528b89674175eb8c9075e01a061a0fd90b6ead73
SoulAndBone <soulandbone@gmail.com>
2023-04-24 19:30:58 +0300

    eliminated the list of questions in HomeScreen, now they are only in the Sports Category Card

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,35 +10,6 @@ import '../widgets/composed_widgets/points_ranking.dart';
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
 
-  final List<int> numbers = [1, 2, 3, 4];
-  final Map<String, String> answers = {
-    '1': 'Wilt Chamberlain',
-    '2': 'Dominique Wilkins',
-    '3': 'Magic Johnson',
-    '4': 'Tracy McGrady'
-  };
-
-  final List<Map<String, dynamic>> questionsAnswers = [
-    {
-      'question': 'Who scored 100 points in a game',
-      'answers': {
-        '1': 'Wilt Chamberlain',
-        '2': 'Dominique Wilkins',
-        '3': 'Magic Johnson',
-        '4': 'Tracy McGrady'
-      },
-    },
-    {
-      'question': 'Who scored 60 in his last game in the NBA',
-      'answers': {
-        '1': 'Kobe Bryant',
-        '2': 'Larry Bird',
-        '3': 'Kareem Abdul-Jabbar',
-        '4': 'Vince Carter'
-      },
-    },
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -69,20 +40,6 @@ class HomeScreen extends StatelessWidget {
                   title: 'Geography', image: 'assets/images/geography.jpg')
             ],
           ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: questionsAnswers[0]['answers']
-                  .length, // Need to see how to cycle through all the Questions with every new Screen. Needs to be dynamic
-              itemBuilder: (context, index) {
-                return Center(
-                    child: Answers(
-                  number: index,
-                  answerText:
-                      '${questionsAnswers[0]['answers']['${index + 1}']}', // The '0' needs to be dynamic for every new page of trivia
-                ));
-              },
-            ),
-          )
         ]),
       ),
     );
